7214b5bbd4491541d745ff0fac20d0647b803485
isro24 <isro.usman.ft22@mail.umy.ac.id>
2023-06-22 11:11:59 +0700

    Jika FRONT_position > REAR_position, maka akan dilakukan iterasi dari FRONT hingga akhir

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -72,13 +72,23 @@ public:
 
 		cout << "\nElement in the queue are: \n";
 
-		// Jika FROT_position <= REAR_position, iterasi dari FRONT hingga REAR
+		// Jika FRONT_position <= REAR_position, iterasi dari FRONT hingga REAR
 		if (FRONT_position <= REAR_position) {
 			while (FRONT_position < -REAR_position) {
 				cout << queue_array[FRONT_position] << "   ";
 				FRONT_position++;
 			}
 			cout << endl;
+		}
+		else {
+			// Jika FRONT_position > REAR_position, iterasi dari FRONT hingga akhir 
+			while (FRONT_position <= max - 1) {
+				cout << queue_array[FRONT_position] << "   ";
+				FRONT_position++;
+			}
+			
+
+			FRONT_position = 0;
 		}
 	}
 };
